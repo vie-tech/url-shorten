9738fa8c292ea73c08acf81c44f7680af28a9046
Ativie Favour <ativiefavour03@gmail.com>
2024-03-25 21:12:30 +0100

    Inclusion of middleware (JSON parser and view engine configuration)

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ const app = express()
 const PORT = process.env.PORT || 7000
 
 
+//MIDDLEWARE SETUP
+app.set('view engine', 'ejs') //View engine to be used for the UI
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+
+
+
+//ROUTES SETUP
+app.get('/', (req, res)=>{
+    
+})
+
+
+
+
+
 
 
 //SERVER LISTENING FUNCTION
