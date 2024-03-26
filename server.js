3c2d9cde4e39887f04a8d1aef6b293197e38632a
Ativie Favour <ativiefavour03@gmail.com>
2024-03-26 21:44:52 +0100

    Creation of decoded route handler and decoded view template

--- server.js
+++ server.js
@@ -2,63 +2,81 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 7000;
-const shortid = require('shortid');
-const url = require('url');
-
+const shortid = require("shortid");
+const url = require("url");
 
 //MIDDLEWARE SETUP
 app.set("view engine", "ejs"); //View engine to be used for the UI
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
- 
 // Store original URLs and their corresponding short URLs
 const urlDatabase = {};
-
-
 
 //ROUTES SETUP
 
 // Route to render the homepage
-app.get('/', (req, res) => {
+// ROUTE TO RENDER THE HOMEPAGE
+app.get("/", (req, res) => {
   const shortenedUrls = Object.keys(urlDatabase);
-  res.render('index', { shortenedUrls, urlDatabase }); // Pass urlDatabase to the template
+ 
+  res.render("index", { shortenedUrls, urlDatabase }); // Pass decodedUrl to the template
 });
 
 
 
-// Route to take you to the original Url destination/* 
-app.get('/:shortUrl', (req, res) => {
-  const shortUrl = req.params.shortUrl;
+//ROUTE TO DECODE URL
+
+app.post('/decode', (req, res) => {
+  const shortUrl = req.body.url;
   const originalUrl = urlDatabase[shortUrl] ? urlDatabase[shortUrl].originalUrl : null;
+  res.render('decoded', { decodedUrl: originalUrl }); // Always pass decodedUrl to the template
+});
+
+
+
+
+// Route to take you to the original Url destination/*
+app.get("/:shortUrl", (req, res) => {
+  const shortUrl = req.params.shortUrl;
+  const originalUrl = urlDatabase[shortUrl]
+    ? urlDatabase[shortUrl].originalUrl
+    : null;
   if (originalUrl) {
     urlDatabase[shortUrl].hits++;
     res.redirect(originalUrl);
   } else {
-    res.status(404).send('URL not found');
+    res.status(404).send("URL not found");
   }
 });
- 
+
+
+
+
 //Route to get the statistics of the clicked URL
-app.get('/statistics/:short_url', (req, res)=>{
+app.get("/statistics/:short_url", (req, res) => {
   const short_url = req.params.short_url;
-  const clicks =  urlDatabase[short_url].hits++;
-  const originalUrl = urlDatabase[short_url] ? urlDatabase[short_url].originalUrl : null;
+  const clicks = urlDatabase[short_url].hits++;
+  const originalUrl = urlDatabase[short_url]
+    ? urlDatabase[short_url].originalUrl
+    : null;
   const parsedUrl = new URL(originalUrl);
   if (originalUrl) {
-   res.render('statistics', {clicks:clicks, originalUrl, parsedUrl: parsedUrl.host})
-  }else{
-    res.send('SOMETHING WENT WRONG WITH THE REQUEST')
+    res.render("statistics", {
+      clicks: clicks,
+      originalUrl,
+      parsedUrl: parsedUrl.host,
+    });
+  } else {
+    res.send("SOMETHING WENT WRONG WITH THE REQUEST");
   }
- 
-})
+});
 
 
 
 //ROUTE TO SHORTEN THE URL
-app.post('/encode', (req, res) => {
-  console.log(req.hostname)
+app.post("/encode", (req, res) => {
+  console.log(req.hostname);
   const originalUrl = req.body.url;
   const shortUrl = generateShortUrl();
   urlDatabase[shortUrl] = { originalUrl, hits: 0 };
@@ -67,10 +85,14 @@ app.post('/encode', (req, res) => {
 
 
 
+
+
 // Generate a short ID for URL
 function generateShortUrl() {
   return shortid.generate();
 }
+
+
 
 //SERVER LISTENING FUNCTION
 app.listen(PORT, () => {
