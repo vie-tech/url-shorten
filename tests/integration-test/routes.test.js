547a150bfe45735d532f103d7d29bb6aa1c6395f
Ativie Favour <ativiefavour03@gmail.com>
2024-03-27 20:22:05 +0100

    Inclusion of statistics route tests and error confirmation

--- tests/integration-test/routes.test.js
+++ tests/integration-test/routes.test.js
@@ -2,6 +2,7 @@ const server = require("../../server");
 const request = require("supertest");
 const {urlDatabase} = require('../../routes/encoder')
 
+//ENCODE ROUTES
 describe("ENCODING ROUTES TESTS", () => {
   describe("TESTING POSITIVE OUTCOMES", () => {
     it("GET / should return homepage", async () => {
@@ -31,6 +32,8 @@ describe("ENCODING ROUTES TESTS", () => {
 )});
 
 
+
+//DECODE ROUTES
 describe('Decode URL Route', () => {
     beforeEach(() => {
       urlDatabase['shortUrl123'] = { originalUrl: 'https://example.com', hits: 0 };
@@ -56,6 +59,31 @@ describe('Decode URL Route', () => {
     });
   });
   
+
+
+
+
+  describe('Statistics Route Tests', () => {
+    beforeEach(() => {
+      urlDatabase['shortUrl123'] = { originalUrl: 'https://example.com', hits: 5 }; // Sample data for testing
+    });
+  
+    test('Fetching statistics for existing short URL', async () => {
+      const response = await request(server)
+        .get('/statistics/shortUrl123')
+        .expect(200);
+  
+      expect(response.text).toContain(' <title>Route statistics</title>');
+    });
+  
+    test('Fetching statistics for non-existent short URL', async () => {
+      const response = await request(server)
+        .get('/statistics/nonExistentShortUrl')
+        .expect(500);
+  
+      expect(response.text).toContain('<title>Error</title>');
+    });
+  });
   
   
   
