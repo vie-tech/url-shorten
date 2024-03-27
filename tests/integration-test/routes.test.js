dcfa097e3751d4c7f7299195a284731da1aa91cc
Ativie Favour <ativiefavour03@gmail.com>
2024-03-27 18:42:01 +0100

    Testing the homepage rendering and making sure it initializes with a status code 200 for successfull processing

--- tests/integration-test/routes.test.js
+++ tests/integration-test/routes.test.js
@@ -3,5 +3,12 @@ const request = require('supertest')
 
 
 describe('Encoding and decoding routes', ()=>{
-    
+    // Test homepage rendering
+  it('GET / should return homepage', async () => {
+    const res = await request(server).get('/');
+    expect(res.status).toBe(200);
+    expect(res.text).toContain('<!DOCTYPE html>');
+  });
+
+  
 })
